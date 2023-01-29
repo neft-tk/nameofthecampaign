599c4d59736f6ffb14a7a419763ecf9dc19d5c3a
unknown <ebussman@gmail.com>
2023-01-28 18:21:26 -0800

    used moment to actually change the format of the time

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,16 +1,20 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import moment from 'moment'
 
 
 export default function Post({ postTitle, postText, createdAt }) {
   const [editedTime, setEditedTime] = useState('')
 
-  
+  useEffect(() => {
+    setEditedTime(moment(createdAt).format('MM/DD/YYYY'))
+  },[])
+
+
     return (
       <div>
         <h1>{postTitle}</h1>
         <p>{postText}</p>
-        <p>{createdAt}</p>
+        <p>{editedTime}</p>
       </div>
     )
 }
